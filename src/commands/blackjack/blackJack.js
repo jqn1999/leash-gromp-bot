4533bc94281ab5eb86c9abdb52cadf4e5ab0683d
Tomcodess <tom_codes@outlook.com>
2024-02-12 22:40:53 -0500

    i think im doing this right

--- src/commands/blackjack/blackJack.js
+++ src/commands/blackjack/blackJack.js
@@ -1,3 +1,23 @@
 const { ApplicationCommandOptionType } = require("discord.js");
 const { getUserInteractionDetails } = require("../../utils/helperCommands")
 const dynamoHandler = require("../../utils/dynamoHandler");
+
+
+module.exports = {
+    name: "blackjack",
+    description: "Allows a user to play blackjack to win or lose potatos",
+    devOnly: false,
+    // testOnly: false,
+    deleted: false,
+    options: [
+        {
+            name: 'amount',
+            description: 'Amount of potatos to bet',
+            required: true,
+            type: ApplicationCommandOptionType.Number,
+        }
+    ],
+    callback: async (client, interaction) => {
+    
+    }
+}
