a3d57743405ca245ecc1ff868fe7632242bd8a87
Joshua Nguyen <jnguyensk@gmail.com>
2024-03-27 20:19:13 -0400

    Start adding mob in, have reward be potato and stat upgrades split among raiders

--- src/utils/worldFactory.js
+++ src/utils/worldFactory.js
@@ -1,5 +1,4 @@
 const dynamoHandler = require("./dynamoHandler");
-const { EmbedBuilder } = require("discord.js");
 const { RaidFactory } = require("./raidFactory");
 const { EmbedFactory } = require("./embedFactory");
 const startRaid = require("../commands/guilds/startRaid.js")
@@ -52,7 +51,7 @@ async function startWorldBoss(world, mob){
     const successChance = determinRaidSuccessChance(totalMultiplier, mob.difficulty);
     const successfulRaid = determineRaidResult(successChance);
     if (successfulRaid) {
-        totalRaidReward = Math.round(mob.reward * randomMultiplier);
+        totalRaidReward = Math.round(mob.potatoReward * randomMultiplier);
         splitRaidReward = await raidFactory.handleRaid(raidList, totalRaidReward);
         raidResultDescription = mob.successDescription;
     } else {
@@ -71,13 +70,16 @@ function determinRaidSuccessChance(totalMultiplier, raidDifficulty) {
 // TODO: PUT REAL BOSSES IN
 const worldBossMobs = [
     {
-        name: "dababy PULLS UP IN HIS CONVERTIBLE!",
+        name: "Griseous, the Dragon Fruit",
         thumbnailUrl: "https://cdn.discordapp.com/attachments/1146091052781011026/1206040896672370759/cover4.png?ex=65da901c&is=65c81b1c&hm=3c2f67f963960013fd5cecf2fcf8e79a8b0a8c32e12f157fbc2e2fcc24d3c406&",
-        description: `lyrical autist\n\nJoin this world raid with /join-world-raid!`,
-        successDescription: `you turned him into a convertible congradulations`,
-        failureDescription: `he shot you in a walmart :SKULL:`,
-        reward: 10000000,
-        difficulty: 200
+        description: `Once a loyal servant to the Potato Kingdom, the Dragon Fruit Griseous was banished for its violence. It retreated to a warped dimension on the flipside of the vegetable realm, where common knowledge is distorted and strange. Having gazed upon the prosperous kingdom silently for years, the exiled Griseous has grown bitter with resentment. Now, this ferocious serpent descends upon the land once more, threatening to reign fiery vengeance and enshroud the world in eternal darkness.\n\nJoin this world raid with /join-world-raid!`,
+        successDescription: `The heroes sunder the monstrous Dragon Fruit from the sky, sending Griseous plummeting toward the Earth. Their foe, now grounded, adapts quickly by firmly planting its six legs and bellowing shadowy flames. Guarding against the devastating dark onslaught, the potato adventurers execute a valiant maneuver that silences the unruly renegade. The Dragon Fruit sinks back into its shadowy exile, leaving the Potato Kingdom safe from its vengeance… for now.`,
+        failureDescription: `Despite their best efforts, the potato adventurers are no match for Griseous’s spectral assault of shadowy flames. Succumbing to their foe’s merciless onslaught, the inhabitants of the potato kingdom are forced to seek shelter as their Kingdom is laid to waste by the rancorous Dragon Fruit.`,
+        potatoReward: 20000000,
+        multiReward: 5,
+        passiveReward: 5000000,
+        bankReward: 20000000,
+        difficulty: 800
     }
 
 ]
